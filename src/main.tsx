ba4c80b547c5fb838419124238a819fad991c533
Serik Urazmaxambetov <bytecode.vet@gmail.com>
2025-05-07 21:25:40 +0500

    chore: add telegram web app types

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { type Telegram } from "@twa-dev/types";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+declare global {
+  interface Window {
+    Telegram: Telegram;
+  }
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
