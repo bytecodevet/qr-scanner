1a4f9803dceae50f3ef999f24f1544958d991943
Serik Urazmaxambetov <bytecode.vet@gmail.com>
2025-05-07 22:14:54 +0500

    feature: add qr scan button and handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { useCallback, useEffect } from "react";
+import "./App.css";
+
 export default function App() {
-  return <>Hello World</>;
+  const webApp = window.Telegram.WebApp;
+
+  const showQrScanPopup = useCallback(() => {
+    webApp.showScanQrPopup({ text: "Scan qr code" }, (data) => {
+      webApp.sendData(data);
+      webApp.closeScanQrPopup();
+      webApp.close();
+    });
+  }, [webApp]);
+
+  useEffect(() => {
+    showQrScanPopup();
+  }, [showQrScanPopup]);
+
+  return (
+    <div className="container">
+      <button className="button" onClick={showQrScanPopup}>
+        QR Scanner
+      </button>
+    </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { type Telegram } from "@twa-dev/types";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 
 declare global {
   interface Window {
